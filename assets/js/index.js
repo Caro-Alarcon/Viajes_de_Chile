026ceab3e2401bdf952b5117571dc8dd13624388
Carola <user@users-MacBook-Pro.local>
2023-06-26 16:41:11 -0400

    incorporacion formulario

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+// tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// form submit
+document.getElementById("formulario").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const date = new Date(Date.now());
+    const popup = document.getElementById('popup');
+    document.getElementById("date").textContent = date.toLocaleString();
+    const toast = bootstrap.Toast.getOrCreateInstance(popup);
+    toast.show()
+    setTimeout(() => {
+        toast.hide()
+    }, 2500)
+    e.target.reset();
+})
